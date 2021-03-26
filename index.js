5eca01c43e84c0d4cdce4835c1dafb7e288176a7
Aleksandra <marenkowasasha@icloud.com>
2021-03-26 22:19:12 +0900

    Complited lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Your code here
+function moveDodgerLeft() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left > 0) {
+      dodger.style.left = `${left - 1}px`;
+    }
+  } 
+  function moveDodgerRight() {
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers, 10);
+  
+    if (left < 360) {
+      dodger.style.left = `${left +1}px`;
+    }
+  } 
+document.addEventListener("keydown", function(event) {
+    //alert(event.key);
+    if(event.key=="ArrowRight"){
+        moveDodgerRight();
+
+    }
+    if(event.key=="ArrowLeft"){
+        moveDodgerLeft();
+    }
+  }); 
+  
